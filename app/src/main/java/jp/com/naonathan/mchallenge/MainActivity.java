4cb5b85078c78efcee80f1ba4b22aa74fa6e7fd0
NaotoOta <nao.tennis.tu@gmail.com>
2015-11-14 18:58:51 +0900

    WIP

--- app/src/main/java/jp/com/naonathan/mchallenge/MainActivity.java
+++ app/src/main/java/jp/com/naonathan/mchallenge/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import jp.com.naonathan.shakableswiperecyclerview.ShakableSwipeRecyclerViewAdapter;
-import jp.com.naonathan.shakableswiperecyclerview.ShakableSwipeRecyclerViewTouchListener;
 
 /**
  * Created by NaotoOta on 2015/11/14.
@@ -16,23 +15,17 @@ import jp.com.naonathan.shakableswiperecyclerview.ShakableSwipeRecyclerViewTouch
  */
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private RecyclerView mRecyclerView;
-    private ShakableSwipeRecyclerViewAdapter mShakableSwipeRecyclerViewAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private ShakableSwipeRecyclerViewTouchListener mShakableSwipeRecyclerViewTouchListener;
-    private String[] mDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mLayoutManager = new LinearLayoutManager(this);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mDataset = new String[]{"A", "B", "C"};
-        mShakableSwipeRecyclerViewAdapter = new ShakableSwipeRecyclerViewAdapter(mDataset, R.layout.list_row, R.id.front);
+        String[] mDataset = new String[]{"A", "B", "C", "D", "E", "F"};
+        ShakableSwipeRecyclerViewAdapter mShakableSwipeRecyclerViewAdapter = new ShakableSwipeRecyclerViewAdapter(mDataset, R.layout.list_row, R.id.front);
         mRecyclerView.setAdapter(mShakableSwipeRecyclerViewAdapter);
     }
 }
--- library/src/main/java/jp/com/naonathan/shakableswiperecyclerview/ShakableSwipeRecyclerViewAdapter.java
+++ library/src/main/java/jp/com/naonathan/shakableswiperecyclerview/ShakableSwipeRecyclerViewAdapter.java
@@ -1,17 +1,13 @@
 package jp.com.naonathan.shakableswiperecyclerview;
 
-import android.app.Activity;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
-import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by NaotoOta on 2015/11/14.
@@ -25,6 +21,8 @@ public class ShakableSwipeRecyclerViewAdapter extends RecyclerView.Adapter<Shaka
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnTouchListener {
         private int frontViewId;
+        private int x = 0;
+        private boolean move_enabled = false;
 
         public ViewHolder(View view, int frontViewId) {
             super(view);
@@ -32,45 +30,88 @@ public class ShakableSwipeRecyclerViewAdapter extends RecyclerView.Adapter<Shaka
             view.setOnTouchListener(this);
         }
 
+        // TODO 条件文きれいに
+        // TODO 設定をライブラリ外からできるようにgetter,setterの用意
+        // TODO なめらかに動く値の調査
+        // TODO アニメーションの謎解明
+
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            int x = 0;
-            View frontView = v.findViewById(frontViewId);
-            switch (MotionEventCompat.getActionMasked(event)) {
-                case MotionEvent.ACTION_DOWN: {
-                    TranslateAnimation translateAnimation = new TranslateAnimation(50, 0, 0, 0);
-                    translateAnimation.setDuration(200);
-                    frontView.startAnimation(translateAnimation);
-                    x = (int) event.getX();
-                }
-
-                case MotionEvent.ACTION_UP: {
-                    int deltaX = (int) event.getX() - x;
-                    deltaX = deltaX/10;
-                    if(deltaX < frontView.getWidth()/2) {
-                    } else {
-                        frontView.setVisibility(View.INVISIBLE);
+            final View frontView = v.findViewById(frontViewId);
+            if (frontView.getVisibility() == View.VISIBLE) {
+                switch (MotionEventCompat.getActionMasked(event)) {
+                    case MotionEvent.ACTION_DOWN: {
+                        TranslateAnimation translateAnimation = new TranslateAnimation(50, 0, 0, 0);
+                        translateAnimation.setDuration(200);
+                        frontView.startAnimation(translateAnimation);
+                        x = (int) event.getX();
+                        move_enabled = true;
                     }
-                }
 
-                case MotionEvent.ACTION_MOVE: {
-                    int deltaX = (int) event.getX() - x;
-                    deltaX = deltaX/10;
-                    frontView.layout((int) frontView.getX()+deltaX, (int) frontView.getY(), (int) frontView.getX()+frontView.getWidth(), (int) frontView.getY()+frontView.getHeight());
+                    case MotionEvent.ACTION_UP: {
+                        if (move_enabled) {
+                            int deltaX = (int) event.getX() - x;
+                            // TODO この値は怪しい
+                            if (deltaX < frontView.getWidth() /2) {
+                                frontView.layout(0, (int) frontView.getY(), frontView.getWidth(), (int) frontView.getY() + frontView.getHeight());
+                            } else {
+                                TranslateAnimation translateAnimation = new TranslateAnimation(frontView.getWidth(), 0, frontView.getWidth()*2, 0);
+                                translateAnimation.setDuration(200);
+                                translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                                    @Override
+                                    public void onAnimationStart(Animation animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animation animation) {
+                                        frontView.layout(frontView.getWidth(), (int) frontView.getY(), frontView.getWidth()*2, (int) frontView.getY()+frontView.getHeight());
+                                        frontView.setAnimation(null);
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animation animation) {
+
+                                    }
+                                });
+                                frontView.startAnimation(translateAnimation);
+                                frontView.setVisibility(View.GONE);
+                            }
+                            move_enabled = false;
+                        } else {
+                            frontView.layout(0, (int) frontView.getY(), frontView.getWidth(), (int) frontView.getY() + frontView.getHeight());
+                        }
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+                        int deltaX = (int) event.getX() - x;
+                        if (move_enabled) {
+                            if (deltaX > 0) {
+                                frontView.layout(deltaX, (int) frontView.getY(), frontView.getWidth() + deltaX, (int) frontView.getY() + frontView.getHeight());
+                            }
+                        } else {
+                            move_enabled = true;
+                        }
+                    }
                 }
             }
             return true;
         }
     }
 
-    private String[] mDataset;
+    // TODO datasetいらない。adapterごと渡すようにする
+
+    private String[] mDataSet;
     private int mViewId;
 
-    public ShakableSwipeRecyclerViewAdapter(String[] dataset, int viewId, int frontViewHolder) {
-        mDataset = dataset;
+    public ShakableSwipeRecyclerViewAdapter(String[] dataSet, int viewId, int frontViewHolder) {
+        mDataSet = dataSet;
         mViewId = viewId;
         this.frontViewHolder = frontViewHolder;
     }
+
+
+    // TODO カスタムviewが可能なようにする
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -84,7 +125,7 @@ public class ShakableSwipeRecyclerViewAdapter extends RecyclerView.Adapter<Shaka
 
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataSet.length;
     }
 
 }
